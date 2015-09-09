cec39f68176eaa62459179c9f1b5fab4948aa72a
Giovane Kauer <gfkauer@gmail.com>
2015-09-09 11:01:02 -0300

    Alteração de ambiente
    Alterando para ambiente de produção

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-var baseUrl = 'http://wwwtest.sistemafiergs.org.br:8897/mapws/';
+var baseUrl = 'http://wwwapp.sistemafiergs.org.br:8897/mapws/';
 //var baseUrl = 'http://10.25.12.110:8894/mapws/';
 //var baseUrlAuth = 'http://10.25.12.110:8894/authws/';
-var baseUrlAuth = 'http://wwwtest.sistemafiergs.org.br:8890/authws/';
+var baseUrlAuth = 'http://wwwapp.sistemafiergs.org.br:8890/authws/';
 var itemUrl = baseUrl + 'item/';
 var autenticarUrl = baseUrlAuth + 'autenticar/';
 var logoffUrl =  baseUrlAuth + 'logoff/';
